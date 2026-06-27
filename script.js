27e14a6c5698322fac3b86c8a4c50ef4ae25a1e5
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-27 21:01:40 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function login (){
-return new Promise (function(resolve,reject){
-  setTimeout()
+const result = document.getElementById("result");
+
+function login (success = true){
+return new Promise (function(resolve,reject) {
+  result.innerHTML = "Logging In ...plz wait 3 sec";
+  setTimeout(function() {
+    if (success) {
+      resolve ("Login Succesfull");
+
+    } else {
+      reject ("Login Failed! Invalid username or password.")
+    }
+  },3000);
 });
 }
