8bcd4f84a3f57a52af3bdbbd9bd58d2cfb9cc0ce
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-28 08:04:31 +0530

    updated

--- script.js
+++ script.js
@@ -26,3 +26,64 @@ function login(success = true) {
     });
 
 }
+// ------------------------------
+// Promise Chain
+// ------------------------------
+
+document.getElementById("promiseBtn").addEventListener("click", function () {
+
+    result.innerHTML = "";
+
+    // Random success or failure
+
+    let isSuccess = Math.random() > 0.5;
+
+    login(isSuccess)
+
+        .then(function(message) {
+
+            result.innerHTML = message;
+
+            console.log(message);
+
+        })
+
+        .catch(function(error) {
+
+            result.innerHTML = error;
+
+            console.log(error);
+
+        });
+
+});
+
+// ------------------------------
+// Async / Await
+// ------------------------------
+
+document.getElementById("asyncBtn").addEventListener("click", async function () {
+
+    result.innerHTML = "";
+
+    let isSuccess = Math.random() > 0.5;
+
+    try {
+
+        let message = await login(isSuccess);
+
+        result.innerHTML = message;
+
+        console.log(message);
+
+    }
+
+    catch(error) {
+
+        result.innerHTML = error;
+
+        console.log(error);
+
+    }
+
+});
